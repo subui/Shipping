47a215a231863d85ccf55d00acafaa41566e9559
Su Bui <subui.bk@gmail.com>
2017-03-19 20:08:24 +0700

    form sign up

---
+++ src/Shipping.UI/Scripts/constants.js
@@ -0,0 +1,9 @@
+﻿var constants = {
+    title_sign_up: "Dang ky thanh vien",
+    label_full_name: "Full Name",
+    label_username: "Username",
+    label_password: "Password",
+    label_repassword: "Re-password",
+    label_email: "Email",
+    label_phone: "Phone Number"
+};
