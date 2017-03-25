2d6e38a28b67bbb1d27ffe212bc76fdee720dc3c
subui <subui.bk@gmail.com>
2017-03-25 15:43:47 +0700

    order detail

--- src/Shipping.UI/Scripts/app.js
+++ src/Shipping.UI/Scripts/app.js
@@ -4,12 +4,56 @@ app.run(function ($rootScope) {
     $rootScope.consts = constants;
 });
 
-app.controller('main', function ($scope, $timeout, $mdSidenav) {
+app.config(function ($mdThemingProvider) {
+    $mdThemingProvider.theme('dialogTheme');
+});
+
+app.controller('main', function ($scope, $timeout, $mdSidenav, $mdDialog, $mdToast) {
     $scope.openSidenav = buildToggler('left');
-    
+    $scope.username = '111';
+    $scope.imagePath = 'https://material.angularjs.org/latest/img/washedout.png';
+
+    $scope.showDetail = function (e) {
+        $mdDialog.show({
+            controller: dialogController,
+            templateUrl: '../Templates/order-detail.html',
+            parent: angular.element(document.body),
+            targetEvent: e,
+            clickOutsideToClose: true,
+            fullscreen: true
+        })
+        .then(function () {
+            showToast();
+        }, function () {
+
+        });
+    };
+
+    function showToast() {
+        $mdToast.show(
+            $mdToast.simple()
+                .textContent('Simple Toast')
+                .hideDelay(3000)
+        );
+    };
+
     function buildToggler(componentId) {
-        return function() {
+        return function () {
             $mdSidenav(componentId).toggle();
         };
     };
+
+    function dialogController($scope, $mdDialog) {
+        $scope.hide = function () {
+            $mdDialog.hide();
+        };
+
+        $scope.closeDialog = function () {
+            $mdDialog.cancel();
+        };
+
+        $scope.register = function () {
+            $mdDialog.hide();
+        };
+    }
 });
